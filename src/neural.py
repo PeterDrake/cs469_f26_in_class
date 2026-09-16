d48618c7f2a069bf9cef1a324e235e39c6ff70f3
Peter Drake <drake@lclark.edu>
2026-09-16 13:41:58 -0700

    Partway through learning.

--- src/neural.py
+++ src/neural.py
@@ -1,8 +1,17 @@
 import numpy as np
+from math import exp
+
+def sigmoid(s):
+    return 1 / (1 + exp(-s))
 
 def neuron(inputs):
-    weights = np.array([-0.5, 1, 1])
-    return float(weights@inputs >= 0)
+    # weights = np.array([-1.5, 1, 1])
+    weights = (np.random.random(3) - 0.5) / 100
+    return sigmoid(weights@inputs)
 
-for inputs in (np.array([1, 0, 0]), np.array([1, 0, 1]), np.array([1, 1, 0]), np.array([1, 1, 1])):
+for inputs, teacher in zip((np.array([1, 0, 0]),
+                            np.array([1, 0, 1]),
+                            np.array([1, 1, 0]),
+                            np.array([1, 1, 1])),
+                           (0, 0, 0, 1)):  # Teachers
     print(f'{inputs} -> {neuron(inputs)}')
