199d649b8eaa1821ad5f7c9b9647882a0fa679fe
Peter Drake <drake@lclark.edu>
2026-09-16 13:29:54 -0700

    Changed to NumPy.

--- src/neural.py
+++ src/neural.py
@@ -1,5 +1,8 @@
-def neuron(x1, x2):
-    return float(-0.5 * 1 + 1.0 * x1 + 1.0 * x2 >= 0)
+import numpy as np
 
-for inputs in ((0, 0), (0, 1), (1, 0), (1, 1)):
-    print(f'{inputs} -> {neuron(*inputs)}')
+def neuron(inputs):
+    weights = np.array([-0.5, 1, 1])
+    return float(weights@inputs >= 0)
+
+for inputs in (np.array([1, 0, 0]), np.array([1, 0, 1]), np.array([1, 1, 0]), np.array([1, 1, 1])):
+    print(f'{inputs} -> {neuron(inputs)}')
