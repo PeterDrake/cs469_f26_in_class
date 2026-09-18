cd6422fbfc23a797a15e3ba51b09fb4923811934
Peter Drake <drake@lclark.edu>
2026-09-18 13:36:43 -0700

    Learning for one neuron.

--- src/neural.py
+++ src/neural.py
@@ -1,17 +1,38 @@
 import numpy as np
 from math import exp
 
+inputs = (np.array([1, 0, 0]),
+          np.array([1, 0, 1]),
+          np.array([1, 1, 0]),
+          np.array([1, 1, 1]))
+teachers = (0, 1, 1, 1)
+weights = (np.random.random(3) - 0.5) / 100
+learning_rate = 0.1
+
 def sigmoid(s):
     return 1 / (1 + exp(-s))
 
-def neuron(inputs):
-    # weights = np.array([-1.5, 1, 1])
-    weights = (np.random.random(3) - 0.5) / 100
-    return sigmoid(weights@inputs)
+def neuron(input):
+    return sigmoid(weights@input)
 
-for inputs, teacher in zip((np.array([1, 0, 0]),
-                            np.array([1, 0, 1]),
-                            np.array([1, 1, 0]),
-                            np.array([1, 1, 1])),
-                           (0, 0, 0, 1)):  # Teachers
-    print(f'{inputs} -> {neuron(inputs)}')
+def train(input, teacher):
+    # Run forward
+    activation = neuron(input)
+    # Calculate weight changes
+    change = (-(teacher - activation) *
+              activation *
+              (1 - activation) *
+              input
+              )
+    # Update weights
+    global weights
+    weights = weights - learning_rate * change
+
+for input in inputs:
+    print(f'{input} -> {neuron(input)}')
+print('---')
+for i in range(10000):
+    for input, teacher in zip(inputs, teachers):
+        train(input, teacher)
+for input in inputs:
+    print(f'{input} -> {neuron(input)}')
